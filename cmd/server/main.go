8f038f1cf804a99b8875184ab680e8cbffd9a03e
scgitstuff <67354101+scgitstuff@users.noreply.github.com>
2025-10-26 15:49:02 -0400

    CH2 stub app

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,7 +1,30 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+	"os/signal"
+
+	amqp "github.com/rabbitmq/amqp091-go"
+)
 
 func main() {
+	url := "amqp://guest:guest@localhost:5672/"
+
 	fmt.Println("Starting Peril server...")
+
+	con, err := amqp.Dial(url)
+	if err != nil {
+		fmt.Printf("Bad stuff happened:\n%s\n", err)
+		return
+	}
+	defer con.Close()
+	fmt.Printf("connected to: %s\n", url)
+
+	// wait for ctrl+c
+	signalChan := make(chan os.Signal, 1)
+	signal.Notify(signalChan, os.Interrupt)
+	<-signalChan
+
+	fmt.Println("\nprogram is shutting down")
 }
